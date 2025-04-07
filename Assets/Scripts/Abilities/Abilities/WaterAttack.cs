402986e7085e3926c2beaad3e2ad505f481f409b
Joseph Consoli <114947767+JRavioli@users.noreply.github.com>
2025-04-07 10:40:04 -0500

    Ability+Combo scripts

---
+++ Assets/Scripts/Abilities/Abilities/WaterAttack.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WaterAttack", menuName = "ScriptableObjects/WaterAttack")]
+public class WaterAttack : Ability
+{
+    public override void TriggerAbility()
+    {
+        base.TriggerAbility();
+    }
+}
