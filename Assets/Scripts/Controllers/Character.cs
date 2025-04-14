c5471644d28ddf11a573a3efc0ad55a12886f4e2
JBBateman <jbateman2@mail.bradley.edu>
2025-04-14 11:08:39 -0500

    Movement Range Static

--- Assets/Scripts/Controllers/Character.cs
+++ Assets/Scripts/Controllers/Character.cs
@@ -25,6 +25,8 @@ public class Character : MonoBehaviour
     [SerializeField] private Ability[] attacks;
     private Ability curAbility;
 
+    private Vector2Int startCoordinates;
+
     [SerializeField] private controller controllerType;
     //Set an enum for elemental afflictions to check against combos
 
@@ -52,6 +54,7 @@ public class Character : MonoBehaviour
         Tile.TileSelected += MoveOrAttack;
 
         GameManager.OnTurnStart += TryTriggerCombo;
+        GameManager.OnTurnStart += SetStartCoordinates;
 
         curTile.SetIsOccupied(true);
         curTile.SetOccupyingCharacter(this);
@@ -62,6 +65,7 @@ public class Character : MonoBehaviour
         OnPlayerSelected -= SelectCharacter;
 
         GameManager.OnTurnStart -= TryTriggerCombo;
+        GameManager.OnTurnStart -= SetStartCoordinates;
     }
 
     #endregion
@@ -196,7 +200,7 @@ public class Character : MonoBehaviour
             throw new Exception("No controller assigned to this character.");
 
         Tile.ResetTiles?.Invoke();
-        OnShouldUpdateTiles?.Invoke(moveRange, curTile.GetCoordinates());
+        OnShouldUpdateTiles?.Invoke(moveRange, startCoordinates);
         isSelected = true;
 
         UISetup();
@@ -387,6 +391,11 @@ public class Character : MonoBehaviour
             combo.TriggerCombo();
             Debug.Log("Combo Triggered");
         }
+    }
+
+    private void SetStartCoordinates()
+    {
+        startCoordinates = curTile.GetCoordinates();
     }
 
     private void OnDestroy()
